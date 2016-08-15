04027b4a27404151e96b2a823120a78a39bb0137
knshetty <knshetty@live.com>
2016-08-15 10:50:24 +0300

    Feature: Add Windows-OS serail port handler

--- Unity3d-SerialPortMessanger/Assets/testscript.cs
+++ Unity3d-SerialPortMessanger/Assets/testscript.cs
@@ -3,78 +3,101 @@ using UnityEngine.UI;
 using System.Collections;
 using System.IO.Ports;
 
-public class testscript : MonoBehaviour {
+public class testscript : MonoBehaviour
+{
 
-	public Button toogleBtn;
-	public Text toggleBtnTxt;
-	public Image led;
-	public Text diagnoticTxt;
+    public Button toogleBtn;
+    public Text toggleBtnTxt;
+    public Image led;
+    public Text diagnoticTxt;
 
-	SerialPort portToMsg;
-	bool toggleState;
+    SerialPort portToMsg;
+    bool toggleState;
 
-	void OnEnable()
-	{
-		toggleState = false;
-		toggleBtnTxt.text = "LED Switch";
-		diagnoticTxt.text = "Debug Info...";
-	}
+    void OnEnable()
+    {
+        toggleState = false;
+        toggleBtnTxt.text = "LED Switch";
+        diagnoticTxt.text = "Debug Info...";
+    }
 
-	// Use this for initialization
-	void Start ()
-	{
-		string[] portNames;
-		portNames = System.IO.Ports.SerialPort.GetPortNames();
-		string portNameExtracted = portNames[0].ToString();
-		diagnoticTxt.text = portNameExtracted;
+    // Use this for initialization
+    void Start()
+    {
+        // --- Initialise serail port for a specific OS ---
+        string portNameExtracted = GetPortName("Windows");
+        diagnoticTxt.text = portNameExtracted;
+        portToMsg = new SerialPort(portNameExtracted, 9600, Parity.None, 8, StopBits.One);
 
-		//portToMsg = new SerialPort ("/dev/ttyUSB0", 9600, Parity.None, 8, StopBits.One);
-		//portToMsg = new SerialPort ("/dev/bus/usb/003/002", 9600, Parity.None, 8, StopBits.One);
-		portToMsg = new SerialPort (portNameExtracted, 9600, Parity.None, 8, StopBits.One);
+        // --- Open serial port & send a initialisation msg ---
+        portToMsg.Open();
+        portToMsg.Write("0");
 
-		portToMsg.Open ();
-		portToMsg.Write ("0");
+        // Initialise UI-LED indicator color
+        led.color = Color.clear;
+    }
 
-		led.color = Color.clear;
-	}
-	
-	// Update is called once per frame
-	void Update ()
-	{
-		if (toggleState)
-		{
-			portToMsg.WriteLine("1");
-		}
-		else
-		{
-			portToMsg.WriteLine("0");
-		}
-	}
+    // Update is called once per frame
+    void Update()
+    {
+        if (toggleState)
+        {
+            portToMsg.WriteLine("1");
+        }
+        else
+        {
+            portToMsg.WriteLine("0");
+        }
+    }
 
-	void OnDisable ()
-	{
-		// --- Clean-up & degrade ---
-		portToMsg.Write ("0");
-		portToMsg.Close();
-	}
+    void OnDisable()
+    {
+        // --- Clean-up & degrade ---
+        portToMsg.Write("0");
+        portToMsg.Close();
+    }
 
-	// --------------
-	// Public Methods
-	// --------------
+    // --------------
+    // Public Methods
+    // --------------
 
-	public void OnClick()
-	{
-		if (toggleState)
-		{
-			toggleState = false;
-			toggleBtnTxt.text = "Off";
-			led.color = Color.clear;
-		}
-		else
-		{
-			toggleState = true;
-			toggleBtnTxt.text = "On";
-			led.color = Color.red;
-		}
-	}
+    public void OnClick()
+    {
+        if (toggleState)
+        {
+            toggleState = false;
+            toggleBtnTxt.text = "Off";
+            led.color = Color.clear;
+        }
+        else
+        {
+            toggleState = true;
+            toggleBtnTxt.text = "On";
+            led.color = Color.red;
+        }
+    }
+
+    // ---------------
+    // Private Methods
+    // ---------------
+
+    private string GetPortName(string os)
+    {
+        switch (os)
+        {
+            case "Linux":
+                return "/dev/ttyUSB0";
+
+            case "Windows":
+                return "COM8";
+
+            case "Android":
+                string[] portNames;
+                portNames = System.IO.Ports.SerialPort.GetPortNames();
+                return portNames[0].ToString();
+
+            default:
+                return "";
+        }
+    }
 }
